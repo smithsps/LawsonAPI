f0e640c2b37f309ff2bba1f87dae00e53146a673
Simon Smith <smithsps@gmail.com>
2013-12-03 16:14:14 -0500

    Started SQLite3 database. Though doubts occur..

--- src/discover.py
+++ src/discover.py
@@ -3,16 +3,37 @@ import User
 import sqlite3 as sqlite
 import logging
 
-//Builds the (new) database.
+//Builds the (new) sqlite3 database.
 def build(){
+    connection = sqlite.connect("new_discover.db")
+    c = connection.cursor()
+    
     //table users
     //  username, name, timestamp, location(s)
+    c.execute('''CREATE TABLE users 
+        (name text, username text, timestamp text, locations blob)''')
+    
+    
+    
+    connection.commit();
     //table computers
     //  name, inuse(yes,no,sshed), primaryuser, users
+    
+    c.execute('''CREATE TABLE computers 
+        (name text, inuse text, users blob)''')
+    connection.commit();
     //table names
         //lookup table
+    c.execute('''CREATE TABLE computers 
+        (name text, inuse text, users blob)''')    
+    connection.commit();    
     //table info
         //id, details
+    c.execute('''CREATE TABLE computers 
+        (id text, details blob)''')
+    connection.commit();
+    connection.close();
+    
 }
 
 
