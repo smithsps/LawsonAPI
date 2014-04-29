0df13d81057fea25ecf157010ce58ef91d1ad98c
smithsp <smithsp@purdue.edu>
2014-04-28 20:56:02 -0400

    Fixed tabbing

--- src/discover_users.py
+++ src/discover_users.py
@@ -3,15 +3,15 @@ import sqlite3 as sqlite
 
 #Builds a list of all users from /etc/passwd on lore.
 def build():
-  ssh = subprocess.Popen(['ssh', 'lore', 'cat', '/etc/passwd'], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  raw = ssh.stdout.readlines()
-  
-  lookup = []
-  for line in raw:
-    rawSplit = line.split(":")
-    lookup.append((rawSplit[0], rawSplit[4].strip(',')))
+	ssh = subprocess.Popen(['ssh', 'lore', 'cat', '/etc/passwd'], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	raw = ssh.stdout.readlines()
 
-  return lookup
+	lookup = []
+	for line in raw:
+		rawSplit = line.split(":")
+		lookup.append((rawSplit[0], rawSplit[4].strip(',')))
+
+	return lookup
 
 
 def execute(db_name):
